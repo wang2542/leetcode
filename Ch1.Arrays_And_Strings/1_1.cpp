9aad5344422365600473be319a19c2a91d164335
JINGZHOU WANG <wang2542@purdue.edu>
2020-02-26 11:46:37 +0800

    1.1 bitset sol

--- Ch1.Arrays_And_Strings/1_1.cpp
+++ Ch1.Arrays_And_Strings/1_1.cpp
@@ -1,3 +1,4 @@
+// --------------------------------------------------------
 class Solution {
 public:
     bool isUnique(string astr) {
@@ -16,5 +17,29 @@ public:
     }
 };
 
-* https://www.geeksforgeeks.org/unordered_map-in-cpp-stl/
-* hash map 方法 （散列表）
+// https://www.geeksforgeeks.org/unordered_map-in-cpp-stl/
+// hash map 方法 （散列表）
+// --------------------------------------------------------
+
+class Solution {
+public:
+    bool isUnique(string astr) {
+        bitset<256> bits(0);
+	for(int i = 0; i < astr.length(); i++) {
+		int val = astr[i];
+		if(bits.test(val) > 0) {
+			return false;
+		}
+		bits.set(val);
+	}
+	return true;
+    }
+};
+
+
+// https://baike.baidu.com/item/BitSet/5816051?fr=aladdin
+// https://www.cnblogs.com/magisk/p/8809922.html
+// 位向量
+// 测试某个单独的位是否为1 也有两种方式
+// 1. test()操作。用位置做参数
+// 2. 我们也可以用下标操作符
